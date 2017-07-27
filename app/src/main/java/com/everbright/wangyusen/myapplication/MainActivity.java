6eebe767574a4736eab005c7f0d71bf80e7f1337
everbrightw <yusencs@163.com>
2017-07-27 15:07:16 -0400

    service run in bacground

--- app/src/main/java/com/everbright/wangyusen/myapplication/MainActivity.java
+++ app/src/main/java/com/everbright/wangyusen/myapplication/MainActivity.java
@@ -1,14 +1,65 @@
 package com.everbright.wangyusen.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.everbright.wangyusen.myapplication.assitantui.UIService;
+
+import static android.R.attr.permission;
 
 
 public class MainActivity extends AppCompatActivity {
+    public final static int REQUEST_CODE = 23456;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)); use this to check user permission
+        checkDrawOverlayPermission();
+
+
+
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void checkDrawOverlayPermission() {
+        /** check if we already  have permissionto draw over other apps */
+        if (!Settings.canDrawOverlays(this)) {
+            /** if not construct intent to request permission */
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            /** request permission via start activity for result */
+            startActivityForResult(intent, REQUEST_CODE);
+            Log.i("test", "2");
+        }
+        else{
+            Log.i("test", "passed");
+
+            startService(new Intent(getBaseContext(), UIService.class));
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        /** check if received result code
+         is equal our requested code for draw permission  */
+
+        if (requestCode == REQUEST_CODE) {
+
+            if (Settings.canDrawOverlays(this)) {
+                // continue here - permission was granted
+                Log.i("entered", "permission=");
+                startService(new Intent(this, UIService.class));
+
+            }
+        }
     }
 }
--- app/src/main/java/com/everbright/wangyusen/myapplication/assitantui/UIService.java
+++ app/src/main/java/com/everbright/wangyusen/myapplication/assitantui/UIService.java
@@ -1,9 +1,96 @@
 package com.everbright.wangyusen.myapplication.assitantui;
 
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.IBinder;
+
+import android.support.annotation.RequiresApi;
+
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+
+import com.everbright.wangyusen.myapplication.MainActivity;
+import com.everbright.wangyusen.myapplication.R;
+
+
 /**
  * Created by wangyusen on 7/26/17.
  */
 
-public class UIService {
-    
+public class UIService extends Service {
+
+    private WindowManager windowManager;
+    private ImageView chatHead;
+    public static boolean isServiceRunning = false;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Not used
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        chatHead = new ImageView(this);
+        chatHead.setImageResource(R.drawable.ic_home_white_24px);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager.addView(chatHead, params);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        /**
+         * prevent it from killing it self;
+         */
+        Intent intent_ = new Intent(this, MainActivity.class);
+        intent_.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent_, 0);
+
+        Notification noti = new Notification.Builder(getApplicationContext())
+                .setContentTitle("Pratikk")
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.ic_home_white_24px)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1234, noti);
+
+        return Service.START_STICKY;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        isServiceRunning = false;
+        if (chatHead != null) windowManager.removeView(chatHead);
+    }
 }
+
+
+
+
+
